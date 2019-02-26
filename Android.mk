888a12fa0ef373c2aa7052ce0a803dc6adeafb2c
mcjino <allan.park@nexell.co.kr>
2019-02-26 18:56:22 +0900

    librecovery: add WriteKernelFn, WriteDTBFn, WriteRootFn functions
    This patch add WriteKernelFn, WriteDTBFn, WriteRootFn functions.
    These functions are use to QPART map which used in conv_svma board.
    
    Signed-off-by: mcjino <allan.park@nexell.co.kr>
    Change-Id: If0f2e3ef86906b8cf61ea4c33e764a5e1c471e1d

--- Android.mk
+++ Android.mk
@@ -3,7 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := bootable/recovery
-LOCAL_SRC_FILES := recovery.cpp
+LOCAL_SRC_FILES := recovery.cpp \
+					ext4.cpp
 LOCAL_MODULE := librecovery_updater_nexell
 include $(BUILD_STATIC_LIBRARY)
 endif
